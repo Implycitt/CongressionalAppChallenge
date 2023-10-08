864f667664ae53b60043876e74c10c35c502a569
Implycitt <qgbordelon@gmail.com>
2023-10-08 10:22:12 -0500

    Boilerplate for window

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('src/main.html')
+}
+
+app.whenReady().then(createWindow)

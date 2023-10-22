29cfff32915a306cc67e439e7c78d8d29fd54813
Implycitt <qgbordelon@gmail.com>
2023-10-22 18:07:24 -0500

    weather and requests

--- src/scripts/utils/Weather.js
+++ src/scripts/utils/Weather.js
@@ -1,3 +1,14 @@
-console.log("I am him")
+/* create weather calls using the key that will be stored in .env and have the data stored into a json file that goes in src/assets/json directory*/
 
-console.log("He is literally him");
+import { writeFileSync } from "fs";
+
+const api_url = ""
+
+async function get_data(url) {
+    const res = await fetch(url);
+    const data = await response.json();
+
+    writeFileSync("weather.json", JSON.stringify(data, null, 4));
+}
+
+get_data(api_url);
--- src/scripts/utils/requests.js
+++ src/scripts/utils/requests.js
@@ -1,18 +1,12 @@
-/**
- * You will need to find some other method to store user data (i.e. localStorage). Not files.
- * Unless you want to look for Webpack file support yourself.
- */
+import { writeFileSync } from "fs";
 
-// import { writeFileSync } from "fs";
+const api_url = "https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0"
 
-// // function(url, apiKey) => function("https://example.com", API_KEY);
-// const api_url = "https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0"
+async function get_data(url) {
+    const res = await fetch(url);
+    const data = await response.json();
 
-// async function get_data(url) {
-//     const res = await fetch(url);
-//     const data = await response.json();
+    writeFileSync("data/insight.json", JSON.stringify(data, null, 4));
+}
 
-//     writeFileSync("data/insight.json", JSON.stringify(data, null, 4));
-// }
-
-// get_data(api_url);
+get_data(api_url);
